eb40d6268d00ca8a3ed1589938d465885d06e3f6
Abhinay Jangde <abhinayjangde@gmail.com>
2022-11-13 20:39:46 +0530

    Product sulg is created and all pages are created - codeswags.com

---
+++ pages/caps.js
@@ -0,0 +1,136 @@
+import React from 'react'
+import Link from 'next/link'
+const caps = () => {
+  return (
+    <section className="text-gray-600 body-font">
+    <div className="container px-5 py-24 mx-auto">
+      <div className="flex flex-wrap items-center justify-center -m-4">
+        <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+          <Link href={'/product/wear-the-code'} className="block relative  rounded overflow-hidden">
+            <img alt="ecommerce" className=" block " src="https://m.media-amazon.com/images/I/91+yD3UV+jL._UX679_.jpg" />
+            <div className="mt-4">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                T-SHIRTS
+              </h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">
+                Wear the code
+              </h2>
+              <p className="mt-1">₹499</p>
+              <p className="mt-1">Size - S, M, L, XL, XXL</p>
+            </div>
+          </Link>
+        </div>
+        <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+          <Link href={'/product/wear-the-code'} className="block relative  rounded overflow-hidden">
+            <img alt="ecommerce" className=" block " src="https://m.media-amazon.com/images/I/91+yD3UV+jL._UX679_.jpg" />
+            <div className="mt-4">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                T-SHIRTS
+              </h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">
+                Wear the code
+              </h2>
+              <p className="mt-1">₹499</p>
+              <p className="mt-1">Size - S, M, L, XL, XXL</p>
+            </div>
+          </Link>
+        </div>
+        <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+          <Link href={'/product/wear-the-code'} className="block relative  rounded overflow-hidden">
+            <img alt="ecommerce" className=" block " src="https://m.media-amazon.com/images/I/91+yD3UV+jL._UX679_.jpg" />
+            <div className="mt-4">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                T-SHIRTS
+              </h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">
+                Wear the code
+              </h2>
+              <p className="mt-1">₹499</p>
+              <p className="mt-1">Size - S, M, L, XL, XXL</p>
+            </div>
+          </Link>
+        </div>
+        <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+          <Link href={'/product/wear-the-code'} className="block relative  rounded overflow-hidden">
+            <img alt="ecommerce" className=" block " src="https://m.media-amazon.com/images/I/91+yD3UV+jL._UX679_.jpg" />
+            <div className="mt-4">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                T-SHIRTS
+              </h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">
+                Wear the code
+              </h2>
+              <p className="mt-1">₹499</p>
+              <p className="mt-1">Size - S, M, L, XL, XXL</p>
+            </div>
+          </Link>
+        </div>
+        <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+          <Link href={'/product/wear-the-code'} className="block relative  rounded overflow-hidden">
+            <img alt="ecommerce" className=" block " src="https://m.media-amazon.com/images/I/91+yD3UV+jL._UX679_.jpg" />
+            <div className="mt-4">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                T-SHIRTS
+              </h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">
+                Wear the code
+              </h2>
+              <p className="mt-1">₹499</p>
+              <p className="mt-1">Size - S, M, L, XL, XXL</p>
+            </div>
+          </Link>
+        </div>
+        <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+          <Link href={'/product/wear-the-code'} className="block relative  rounded overflow-hidden">
+            <img alt="ecommerce" className=" block " src="https://m.media-amazon.com/images/I/91+yD3UV+jL._UX679_.jpg" />
+            <div className="mt-4">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                T-SHIRTS
+              </h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">
+                Wear the code
+              </h2>
+              <p className="mt-1">₹499</p>
+              <p className="mt-1">Size - S, M, L, XL, XXL</p>
+            </div>
+          </Link>
+        </div>
+        <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+          <Link href={'/product/wear-the-code'} className="block relative  rounded overflow-hidden">
+            <img alt="ecommerce" className=" block " src="https://m.media-amazon.com/images/I/91+yD3UV+jL._UX679_.jpg" />
+            <div className="mt-4">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                T-SHIRTS
+              </h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">
+                Wear the code
+              </h2>
+              <p className="mt-1">₹499</p>
+              <p className="mt-1">Size - S, M, L, XL, XXL</p>
+            </div>
+          </Link>
+        </div>
+        <div className="lg:w-1/5 md:w-1/2 p-4 w-full cursor-pointer shadow-lg m-5">
+          <Link href={'/product/wear-the-code'} className="block relative  rounded overflow-hidden">
+            <img alt="ecommerce" className=" block " src="https://m.media-amazon.com/images/I/91+yD3UV+jL._UX679_.jpg" />
+            <div className="mt-4">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                T-SHIRTS
+              </h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">
+                Wear the code
+              </h2>
+              <p className="mt-1">₹499</p>
+              <p className="mt-1">Size - S, M, L, XL, XXL</p>
+            </div>
+          </Link>
+        </div>
+
+
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default caps
