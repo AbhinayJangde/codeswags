ccab9b145bf073cc20d8142be2187aeb483e31fc
Abhinay Jangde <abhinayjangde@gmail.com>
2024-02-17 14:52:43 +0530

    I add hover effect in home page card

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ const Home = () => {
           </div>
           <div className="flex flex-col md:flex-row -m-4">
             <div className="xl:w-1/3 md:w-1/2 p-4">
-              <div className="border border-gray-200 p-6 rounded-lg flex flex-col items-center justify-center">
+              <div className="border border-gray-200 p-6 rounded-lg flex flex-col items-center justify-center transition-colors duration-300 hover:bg-blue-100">
                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue-400 text-indigo-900 mb-4">
                   <FaTshirt className='text-2xl' />
                 </div>
@@ -37,11 +37,10 @@ const Home = () => {
               </div>
             </div>
             <div className="xl:w-1/3 md:w-1/2 p-4">
-              <div className="border border-gray-200 p-6 rounded-lg flex flex-col items-center justify-center">
+              <div className="border border-gray-200 p-6 rounded-lg flex flex-col items-center justify-center transition-colors duration-300 hover:bg-blue-100">
                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue-400 text-indigo-900 mb-4">
-                  <MdLocalShipping className='text-2xl' />
+                  <MdLocalShipping className="text-2xl" />
                 </div>
-
                 <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
                   Free Shipping
                 </h2>
@@ -50,8 +49,9 @@ const Home = () => {
                 </p>
               </div>
             </div>
+
             <div className="xl:w-1/3 md:w-1/2 p-4">
-              <div className="border border-gray-200 p-6 rounded-lg flex flex-col items-center justify-center">
+              <div className="border border-gray-200 p-6 rounded-lg flex flex-col items-center justify-center transition-colors duration-300 hover:bg-blue-100">
                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue-400 text-indigo-900 mb-4">
                   <BiRupee className='text-2xl' />
                 </div>
